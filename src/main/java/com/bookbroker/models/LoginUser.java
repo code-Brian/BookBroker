44bd11a4d423563af083dc526d0055735209f4e4
Brian Denmark <testingarea479@gmail.com>
2023-01-21 22:25:06 -0800

    added models

---
+++ src/main/java/com/bookbroker/models/LoginUser.java
@@ -0,0 +1,33 @@
+package com.bookbroker.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class LoginUser {
+	@Email
+	@NotNull
+	private String email;
+	
+	@NotNull
+	@Size(min=8, message="Password must be 8 characters!")
+	private String password;
+	
+	public LoginUser() {}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
