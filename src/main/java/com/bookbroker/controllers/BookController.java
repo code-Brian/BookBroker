c385ed60136fc844c1f714a9f731fd00d93f8c16
Brian Denmark <testingarea479@gmail.com>
2023-01-22 23:57:40 -0800

    began workin on book controller

--- src/main/java/com/bookbroker/controllers/BookController.java
+++ src/main/java/com/bookbroker/controllers/BookController.java
@@ -1,5 +1,22 @@
 package com.bookbroker.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bookbroker.models.Book;
+
+@Controller
+@RequestMapping("/book")
 public class BookController {
+	Autowired
+	BookService bookServ;
+	
+	@GetMapping("/create")
+	public String create(@ModelAttribute("book") Book book) {
+		return "createBook.jsp";
+	}
 
 }
